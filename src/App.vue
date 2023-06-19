8edbc60a58c93e41f30fece18b0ffdcd2e4fa183
dimashikalov <dimashikalov86@mail.ru>
2023-06-19 14:37:54 +0400

    created Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-app> </v-app>
+  <v-app>
+    <Navbar />
+  </v-app>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
 export default {
   name: "App",
-
-  data: () => ({
-    //
-  }),
+  components: {
+    Navbar,
+  },
 };
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container fluid>
+    <v-app-bar color="light-blue" dark>
+      <v-spacer />
+      <v-btn outlined class="ml-4">Главная</v-btn>
+      <v-btn outlined class="ml-4">Список дел</v-btn>
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
